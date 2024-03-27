3c5dae460e85b76a79c04009c9a36ccade64ada2
Kaua Kestering <kaua.zilton@gmail.com>
2024-03-27 20:59:20 -0300

    sefundo  commit do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import iconSedans from "./assets/icon-sedans.svg"
+import iconsuvs from "./assets/icon-suvs.svg"
+import iconslux from "./assets/icon-luxury.svg"
+
+
 export default function App() {
   return(
-    <h1>hello word</h1>
+    <main>
+      <section>
+        <div>
+          <img src={iconSedans} alt="icone ccarro amarelo" />
+          <h2>SEDANS</h2>
+          <p>Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
+          <a href="https://www.webmotors.com.br/sedans" target="_blank">Ver mais</a>
+        </div>
+
+        <div>
+          <img src={iconsuvs} alt="icon suvs" />
+          <h2>SUVs</h2>
+          <p>Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
+          <a href="https://www.webmotors.com.br/suvs" target="_blank">Ver mais</a>
+        </div>
+
+        <div>
+          <img src={iconslux} alt="icon lux" />
+          <h2>Luxury</h2>
+          <p>Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
+          <a href="https://www.webmotors.com.br/carros/estoque/bmw/i7?estadocidade=estoque&marca1=BMW&modelo1=i7&autocomplete=i7&autocompleteTerm=BMW%20i7&lkid=1705&idcmpint=t1:c17:m07:webmotors:modelo::bmw%20i7" target="_blank">Ver mais</a>
+        </div>
+
+      </section>
+    </main>
   )
 }
