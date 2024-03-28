c61407ee080f545978645f7a557c0be98700b9e1
Kaua Kestering <kaua.zilton@gmail.com>
2024-03-27 22:59:10 -0300

    PROJETO FINALIZADO

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Luxo from "./components/Luxo"
 export default function App() {
   return(
     <main className=" min-h-svh bg-offwhitebg py-20 px-6 lg:flex justify-center items-center">
-      <section className="lg:flex max-w-[920px]">
+      <div className="lg:flex max-w-[920px]">
         <Sedans />
 
         <Suvs />
@@ -14,7 +14,7 @@ export default function App() {
         <Luxo />
         
 
-      </section>
+      </div>
     </main>
   )
 }
